1625d7992e948988f8fd08bf7a23dd502aabb907
younes313 <you@example.com>
2020-02-02 13:56:53 +0330

    third

--- app.py
+++ app.py
@@ -48,26 +48,43 @@ def calculate(answers):
 
     
 	
-    
+#     #json version
 
-    result = {
-        'answers':answers,
-        'results':dic        
-    }
+#     result = {
+#         'answers':answers,
+#         'results':dic        
+#     }
 
-    # request Odoo here
-    url = 'http://127.0.0.1:5000/get-result-and-return-that' 
-    r = requests.post(url, json = result)
-    print( r.json() )
+#     # request Odoo here
+#     url = 'http://127.0.0.1:5000/get-result-and-return-that' 
+#     r = requests.post(url, json = result)
+#     print( r.json() )
+
+#     return render_template('result.html' , dic=dic)
+
+
+ # query parameter version
+
+    url = 'http://127.0.0.1:5000/gardner/'+ str(dic['Linguistic']) + ',' +str(dic['Logical_mathematical']) + ',' +str(dic['Spatial']) + ',' +str(dic['Bodily_Kinesthetic']) + ',' +str(dic['Intrapersonal']) + ',' +str(dic['Interpersonal']) + ',' +str(dic['Musical']) + ',' +str(dic['Naturalist'])
+
+    r = requests.post(url)
 
     return render_template('result.html' , dic=dic)
 
 
+
+
 # just for test the request correctness
-@app.route('/get-result-and-return-that' , methods=['POST'])
-def get_and_return():
-    return request.get_json()
-    
+@app.route('/gardner/<any>' , methods=['GET','POST'])
+def get_and_return(any):
+    print(any)
+    return any
+
+    # return request.get_json()
+
+
+
+   
 
 
 
